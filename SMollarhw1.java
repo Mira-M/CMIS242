78e72a492579d5690588adb4abb63ded3f54549d
Mira-M <mira.mollar@gmail.com>
2013-10-21 12:20:05 -0400

    Adds fix to Deposit Class
    Refactors Deposit object througout test file.

--- SMollarhw1.java
+++ SMollarhw1.java
@@ -2,34 +2,39 @@ package com.miramollar.hw1;
 
 
 public class SMollarhw1 {
-	private String auditor;
-	private double regOne;
-	private double regTwo;
-	
-	public SMollarhw1(String auditor, double regOne, double regTwo) {
-		this.auditor = auditor;
-		this.regOne = regOne;
-		this.regTwo = regTwo;
-	}// End Constructor
-	
-	public SMollarhw1(String auditor) {
-		this(auditor, 0.0, 0.0);
-	}
-	
-	public String getAuditor() {
-		return auditor;
-	}
-	
-	public double getRegOne () {
-		return regOne;
-	}
-	
-	public double getRegTwo () {
-		return regTwo;
-	}
-	
-	public double getBankDeposit () {
-		return regOne + regTwo;
-	}
 
-}
+	static class Deposit {
+		
+		private String auditor;
+		private double regOne;
+		private double regTwo;
+		
+		public Deposit (String auditor, double regOne, double regTwo){
+			this.auditor = auditor;
+			this.regOne = regOne;
+			this.regTwo = regTwo;
+		}// End Constructor
+		
+		public Deposit (String auditor) {
+			this(auditor, 0.0, 0.0);
+		}
+		
+		public String getAuditor() {
+			return auditor;
+		}
+		
+		public double getRegOne () {
+			return regOne;
+		}
+		
+		public double getRegTwo () {
+			return regTwo;
+		}
+		
+		public double getBankDeposit () {
+			return regOne + regTwo;
+		}
+		
+	}// End class Deposit
+	
+}// End class SMollarhw1
--- SMollarhw1test.java
+++ SMollarhw1test.java
@@ -3,7 +3,7 @@ package com.miramollar.hw1;
 public class SMollarhw1test {
 
 	public static void main(String[] args) {
-		SMollarhw1 october20 = new SMollarhw1("Mira", 32.45, 345.78);
+		Deposit october20 = new Deposit("Mira", 32.45, 345.78);
 		System.out.println("The deposit for " + october20.getAuditor() + " is: \n" 
 				+ "Register 1 Total: " + october20.getRegOne() + ".\n"
 				+ "Register 2 Total: " + october20.getRegTwo() + ".\n"
@@ -14,7 +14,7 @@ public class SMollarhw1test {
 		 * protocols that may stipulate that an entry must be entered even if there 
 		 * was no deposit for that day.
 		 */
-		SMollarhw1 october21 = new SMollarhw1("Shannon");
+		Deposit october21 = new Deposit("Shannon");
 		System.out.println("The deposit for " + october21.getAuditor() + " is: \n"
 				+ "Register 1 Total: " + october21.getRegOne() + ".\n"
 				+ "Register 2 Total: " + october21.getRegTwo() + ".\n"
